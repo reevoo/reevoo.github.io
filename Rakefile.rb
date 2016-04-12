3e7367de10b6cf1b67326a9d249c1e4ed74debaf
Victor Vila Ases <victor.vila@reevoo.com>
2016-04-12 10:18:29 +0100

    Commented SoapboxCommentLinker to allow new posts to be published until Soapbox is fixed.

--- Rakefile.rb
+++ Rakefile.rb
@@ -5,7 +5,9 @@ require 'soapbox_comment_linker'
 desc 'Load content_id for posts without content id, build and serve'
 namespace :jekyll do
   task :serve do
-    SoapboxCommentLinker.link
+    # TODO: Soapbox is not working so we need to avoid generating ids
+    #       (https://github.com/reevoo/reevoo.github.io/issues/90)
+    # SoapboxCommentLinker.link
     sh 'jekyll serve --port 4001'
   end
 end
